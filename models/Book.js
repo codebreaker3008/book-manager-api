6b64fcfdebd182de0bc3f5be9a78e27f7bd15180
Ayush Kumar <142109210+codebreaker3008@users.noreply.github.com>
2025-06-27 12:42:40 +0530

    Update Book.js

--- models/Book.js
+++ models/Book.js
@@ -2,33 +2,10 @@ const mongoose = require("mongoose");
 
 
 const bookSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Title is required"],
-    minlength: 1,
-    maxlength: 150,
-    trim: true
-  },
-  author: {
-    type: String,
-    required: [true, "Author is required"],
-    minlength: 1,
-    maxlength: 100,
-    trim: true
-  },
-  genre: {
-    type: String,
-    required: [true, "Genre is required"],
-    minlength: 1,
-    maxlength: 100,
-    trim: true
-  },
-  publishedYear: {
-    type: Number,
-    required: [true, "Published year is required"],
-    min: [0, "Published year cannot be negative"],
-    max: [new Date().getFullYear(), "Published year cannot be in the future"]
-  }
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  genre: { type: String, required: true },
+  publishedYear: { type: Number, required: true }
 });
 
 module.exports = mongoose.model("Book", bookSchema);
