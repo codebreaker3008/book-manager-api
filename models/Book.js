4376c19b206eb8362c843b5742bf4cbb089c9c6e
Ayush Kumar <142109210+codebreaker3008@users.noreply.github.com>
2025-06-27 12:50:34 +0530

    Update Book.js

--- models/Book.js
+++ models/Book.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 
 const bookSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  genre: { type: String, required: true },
-  publishedYear: { type: Number, required: true }
+  title: { type: String },         // no "required"
+  author: { type: String },
+  genre: { type: String },
+  publishedYear: {}               // accepts any type: number, string, etc.
 });
 
 module.exports = mongoose.model("Book", bookSchema);
