a6fed0502b62f8f4b364b53ea85e6a2ce43d8758
Ayush Kumar <142109210+codebreaker3008@users.noreply.github.com>
2025-06-27 13:05:57 +0530

    main

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get("/", (req, res) => {
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>📚 Book Manager API</title>
+      <title>Book Manager API</title>
       <style>
         body {
           font-family: "Segoe UI", sans-serif;
